22e94808f2d8323358a313e9ed7792c321cf3aa3
Pedro Vinícius <62823394+pedrovinis@users.noreply.github.com>
2023-06-30 23:27:06 -0300

    fix: locale

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:speezer_app/widgets/AudiosRow.dart';
-import 'package:speezer_app/widgets/PlaybackBar.dart';
-import 'package:speezer_app/widgets/SideBar.dart';
-import 'utils/audiomanager.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:speezer_app/widgets/HomePage.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() => runApp(const SpeezerApp());
@@ -17,15 +12,8 @@ class SpeezerApp extends StatelessWidget {
     return MaterialApp(
       title: 'Speezer App',
       debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en', 'US'),
-        Locale('es', 'ES'),
-        Locale('pt', 'BR'),
-      ],
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       theme: ThemeData(
         primaryColor: Colors.black,
         primarySwatch: Colors.deepPurple,
@@ -33,98 +21,7 @@ class SpeezerApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.black,
         fontFamily: 'Arial',
       ),
-      home: const SpeezerHome(),
-    );
-  }
-}
-
-class SpeezerHome extends StatefulWidget {
-  const SpeezerHome({Key? key}) : super(key: key);
-
-  @override
-  _SpeezerHomeState createState() => _SpeezerHomeState();
-}
-
-class _SpeezerHomeState extends State<SpeezerHome> {
-  AudioManager audioManager = AudioManager();
-  PlayerState playerState = PlayerState.paused;
-
-  @override
-  void initState() {
-    super.initState();
-    AudioPlayer audioPlayer = audioManager.audioPlayer;
-
-    audioPlayer.onPlayerStateChanged.listen((_playerState) {
-      setState(() {
-        playerState = _playerState;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    audioManager.disposeAudioPlayer();
-    super.dispose();
-  }
-
-  void playAudio(String source) {
-    audioManager.playAudio(source);
-  }
-
-  void pauseAudio() {
-    audioManager.pauseAudio();
-  }
-
-  void stopAudio() {
-    audioManager.stopAudio();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        centerTitle: false,
-        title: const Text(
-          'Speezer',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      body: Row(
-        children: [
-          const SideBar(),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 8.0),
-                  child: Text(
-                    "test",
-                    // AppLocalizations.of(context)!.playing_now,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                AudiosRow(),
-                AudiosRow(),
-              ],
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: PlaybackBar(
-        audioManager: audioManager,
-        playerState: playerState,
-      ),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/widgets/HomePage.dart
@@ -0,0 +1,97 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:speezer_app/widgets/AudiosRow.dart';
+import 'package:speezer_app/widgets/PlaybackBar.dart';
+import 'package:speezer_app/widgets/SideBar.dart';
+import '../utils/audiomanager.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  AudioManager audioManager = AudioManager();
+  PlayerState playerState = PlayerState.paused;
+
+  @override
+  void initState() {
+    super.initState();
+    AudioPlayer audioPlayer = audioManager.audioPlayer;
+
+    audioPlayer.onPlayerStateChanged.listen((_playerState) {
+      setState(() {
+        playerState = _playerState;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    audioManager.disposeAudioPlayer();
+    super.dispose();
+  }
+
+  void playAudio(String source) {
+    audioManager.playAudio(source);
+  }
+
+  void pauseAudio() {
+    audioManager.pauseAudio();
+  }
+
+  void stopAudio() {
+    audioManager.stopAudio();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        centerTitle: false,
+        title: const Text(
+          'Speezer',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Row(
+        children: [
+          const SideBar(),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: Text(
+                    AppLocalizations.of(context)!.playing_now,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                AudiosRow(),
+                AudiosRow(),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: PlaybackBar(
+        audioManager: audioManager,
+        playerState: playerState,
+      ),
+    );
+  }
+}
