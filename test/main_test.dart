66f54f286c5b25045017af03f8f48b0551580312
Pedro Vinícius <62823394+pedrovinis@users.noreply.github.com>
2023-07-04 20:05:02 -0300

    feat: add-simple-test

--- test/main_test.dart
+++ test/main_test.dart
@@ -5,26 +5,19 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:speezer_app/data/musics.dart';
 
 import 'package:speezer_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Play music test', (WidgetTester tester) async {
     await tester.pumpWidget(const SpeezerApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    //Verify that the player state is reseted and being rendered
+    expect(find.text("0:00"), findsWidgets);
   });
 }
